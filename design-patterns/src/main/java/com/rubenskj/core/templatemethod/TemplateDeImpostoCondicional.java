f42801a2d043d1c527fd5cd6b287c8d1167fad6d
Rubens Kleinschmidt Júnior <36218162+RubensKj@users.noreply.github.com>
2020-10-06 20:35:05 -0300

    removing the possibility of overwrite the calcula method by adding final to it

--- design-patterns/src/main/java/com/rubenskj/core/templatemethod/TemplateDeImpostoCondicional.java
+++ design-patterns/src/main/java/com/rubenskj/core/templatemethod/TemplateDeImpostoCondicional.java
@@ -5,7 +5,7 @@ import com.rubenskj.core.strategy.Imposto;
 
 public abstract class TemplateDeImpostoCondicional implements Imposto {
     @Override
-    public double calcula(Orcamento orcamento) {
+    public final double calcula(Orcamento orcamento) {
         if (deveUsarMaximaTaxacao(orcamento)) {
             return maximaTaxacao(orcamento);
         }
